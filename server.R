231a61c94ac318b9a3c0b9da51490498d825b52e
muamarifqif <96423960+muamarifqif@users.noreply.github.com>
2022-09-29 12:38:21 +0700

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,9 @@
 library(shiny)
+library(ggplot2)
+library(wordcloud)
+library(memoise)
+library(tm)
+library(epos)
 function(input, output, session) {
   
   Var1 <- reactive({
@@ -17,10 +22,23 @@ function(input, output, session) {
     
     return(Var1)
   })
+  observe({
+    updateSelectInput(session=session, 
+                      inputId="valueCol", 
+                      label="Pilih Kolom Nilai: ",
+                      choices = colnames(Var1()))
+    updateSelectInput(session=session, 
+                      inputId="groupCol", 
+                      label="Pilih Kolom Pengelompokan: ",
+                      choices = colnames(Var1())[!sapply(Var1(), is.numeric)])
+  })
   output$table <- renderTable(Var1())
   
+  myCol <- reactive(Var1()[input$valueCol])
+  #myTable <- reactive(table(myCol()))
+  
   GGPlot <- reactive({
-    myGG <- ggplot(mapping = aes(x = Var1()[input$valueCol]))
+    myGG <- ggplot(mapping = aes(x = unlist(myCol())))
                    
     if(input$graphType=="bar"){
       myGG <- myGG + geom_bar()
@@ -31,8 +49,29 @@ function(input, output, session) {
   })
   output$plot<-renderPlot(GGPlot())
   
+  #terms <- reactive({
+    # Change when the "update" button is pressed...
+    #input$update
+    # ...but not for anything else
+  #  isolate({
+  #    withProgress({
+  #      setProgress(message = "Processing corpus...")
+  #      getTermMatrix(myCol())
+  #    })
+  #  })
+  #})
   
+  #wordcloud_rep <- repeatable(wordcloud)
+  #myWC <- reactive({
+  #  wordcloud_rep(names(myTable()), myTable(), min.freq = 1)
+  #})
   
+  #output$wordCloud <- renderPlot({
+  #  v <- terms()
+  #  wordcloud_rep(names(v), v, scale=c(4,0.5),
+  #                min.freq = input$freq, max.words=input$max,
+  #                colors=brewer.pal(8, "Dark2"))
+  #})
   
   
 

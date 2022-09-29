b4ea6aaf6edadd2e7db0f5835423e135c47ad19c
muamarifqif <96423960+muamarifqif@users.noreply.github.com>
2022-09-29 14:24:53 +0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,39 +5,40 @@ library(bslib)
 
 
 ui <- fluidPage(theme = bs_theme(
-          bg = "#B0E0E6", fg = "black", primary = "#87CEEB",
-          base_font = font_google("Space Mono"),
-          code_font = font_google("Space Mono")),
-                                titlePanel("Stat Visualizer"),
-                                  
-                                  sidebarLayout(
-                                    sidebarPanel(
-                                      fileInput("df", "Upload File", 
-                                                accept = c("text/csv", "text/coma-separated-values,text/plain")),
-                                      checkboxInput("header", "Baris pertama kolom", T),
-                                      radioButtons("separator", "Pemisah",
-                                                   c(comma = ',',
-                                                     semicolon = ';',
-                                                     tab = '\t',
-                                                     space = ' '),
-                                                   selected = ";"),
-                                      selectInput("graphType", "Pilih tipe grafik",
-                                                  choices=c("bar", "density")),
-                                      selectInput("valueCol", "Pilih kolom nilai",
-                                                  choices=NULL),
-                                      selectInput("groupCol", "Pilih kolom pengelompokkan",
-                                                  choices=NULL)
-                                  
-                                    ),
-                                    
-                                    mainPanel(
-                                      
-                                      tabsetPanel(
-                                        tabPanel("Data upload", tableOutput("table")),
-                                        tabPanel("Plot", plotOutput("plot")),
-                                        tabPanel("Word Cloud", plotOutput("wordCloud")))
-                                      )
-                                    )
-                                  )
-
-
+  bg = "#B0E0E6", fg = "black", primary = "#87CEEB",
+  base_font = font_google("Space Mono"),
+  code_font = font_google("Space Mono")),
+  titlePanel("Stat Visualizer"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("df", "Upload File", 
+                accept = c("text/csv", "text/coma-separated-values,text/plain")),
+      checkboxInput("header", "Baris pertama kolom", T),
+      radioButtons("separator", "Pemisah",
+                   c(comma = ',',
+                     semicolon = ';',
+                     tab = '\t',
+                     space = ' '),
+                   selected = ";"),
+      selectInput("graphType", "Pilih tipe grafik",
+                  choices=c("bar", "density")),
+      selectInput("valueCol", "Pilih kolom nilai",
+                  choices=NULL),
+      selectInput("groupCol", "Pilih kolom pengelompokkan",
+                  choices=NULL),
+      br(),
+      downloadButton("downloadPlot","Download Plot"),
+      br(),
+      downloadButton("downloadWC","Download WordCloud")
+      
+    ),
+    
+    mainPanel(
+      tabsetPanel(tabPanel("Data upload", tableOutput("table")),
+                  tabPanel("Plot", plotOutput("plot")),
+                  tabPanel("Word Cloud", verbatimTextOutput("wctest"), plotOutput("wordCloud"))
+    )
+    )
+  )
+)

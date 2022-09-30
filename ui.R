cfe0158360f641810360ac856853f28ce13eea1f
Resti Hadiyani Utami <104712998+restihadiyani@users.noreply.github.com>
2022-09-30 12:27:13 +0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,15 +1,19 @@
 library(shiny)
+library(ggplot2)
+library(wordcloud)
+library(memoise)
+library(tm)
+library(epos)
+library(colourpicker)
 library(shinydashboard)
 library(bslib)
 library(colourpicker)
-
-
 
 ui <- fluidPage(theme = bs_theme(
   bg = "#00203FFF", fg = "White", primary = "#ADEFD1FF",
   base_font = font_google("Alata"),
   code_font = font_google("Alata")),
-  titlePanel("Stat Visualizer"),
+  titlePanel("Stat Visualizer!"),
   
   sidebarLayout(
     sidebarPanel(
@@ -28,22 +32,22 @@ ui <- fluidPage(theme = bs_theme(
                   choices=NULL),
       selectInput("groupCol", "Pilih kolom pengelompokkan",
                   choices=NULL),
-      br(),
-      colourInput("warna", label = "Masukan warna chart",
-                  value = "lightblue", returnName = T,
-                  allowTransparent = T),
-      br(),
-      downloadButton("downloadPlot","Download Plot"),
-      br(),
-      downloadButton("downloadWC","Download WordCloud"),
     ),
     
     mainPanel(
       tabsetPanel(tabPanel("Data upload", tableOutput("table")),
-                  tabPanel("Plot", plotOutput("plot")),
-                  tabPanel("Word Cloud", verbatimTextOutput("wctest"), plotOutput("wordCloud"))
+                  tabPanel("Plot",
+                           br(),
+                           downloadButton("downloadPlot","Download Plot"),
+                           plotOutput("plot"),colourInput("warna", label = "Masukan warna chart",
+                                                          value = "lightblue", returnName = T,
+                                                          allowTransparent = T),),
+                  tabPanel("Word Cloud",
+                           br(),
+                           downloadButton("downloadWC","Download WordCloud"),
+                           verbatimTextOutput("wctest"), plotOutput("wordCloud"),),
+                  
       )
     )
   )
 )
-
